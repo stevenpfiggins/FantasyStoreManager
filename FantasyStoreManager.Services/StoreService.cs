e3603d9a772ede60ae7f8944993c9a1e6bcc0be3
stevenpfiggins <sfiggins43@gmail.com>
2018-11-28 14:12:21 -0500

    Added StoreService

---
+++ FantasyStoreManager.Services/StoreService.cs
@@ -0,0 +1,55 @@
+﻿using FantasyStoreManager.Data;
+using FantasyStoreManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyStoreManager.Services
+{
+    public class StoreService
+    {
+        private readonly Guid _userId;
+
+        public StoreService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateStore(StoreCreate model)
+        {
+            var entity = new Store()
+            {
+                OwnerId = _userId,
+                Name = model.Name,
+                Location = model.Location,
+                TypeofStore = model.TypeOfStore
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Stores.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<StoreListItem> GetStores()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Stores.Where(e => e.OwnerId == _userId).Select(e => new StoreListItem
+                {
+                    StoreId = e.StoreId,
+                    Name = e.Name,
+                    Location = e.Location,
+                    TypeOfStore = e.TypeofStore
+                });
+
+                return query.ToArray();
+            }
+        }
+    }
+
+    
+}
