a213e5c550a3f6bdb8eb24948b3d59774ba46779
stevenpfiggins <sfiggins43@gmail.com>
2018-12-21 10:57:23 -0500

    created inventory and product controllers for api

---
+++ FantasyStoreManager.WebApi/Controllers/InventoryController.cs
@@ -0,0 +1,81 @@
+﻿using FantasyStoreManager.Models;
+using FantasyStoreManager.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FantasyStoreManager.WebApi.Controllers
+{
+    public class InventoryController : ApiController
+    {
+        public IHttpActionResult GetAllStores()
+        {
+            var sortOrder = "";
+            StoreService storeService = CreateStoreService();
+            var stores = storeService.GetStores(sortOrder);
+            return Ok(stores);
+        }
+
+        public IHttpActionResult Post(int id, InventoryCreate inventory)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateInventoryService();
+
+            if (!service.CreateInventory(id, inventory))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult GetAllInventory(int id)
+        {
+            var sortOrder = "";
+            InventoryService inventoryService = CreateInventoryService();
+            var inventories = inventoryService.GetStoreInventories(id, sortOrder);
+            return Ok(inventories);
+        }
+
+        public IHttpActionResult Put(InventoryEdit inventory)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateInventoryService();
+
+            if (!service.UpdateInventory(inventory))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateInventoryService();
+
+            if (!service.DeleteInventory(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private InventoryService CreateInventoryService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var inventoryService = new InventoryService(userId);
+            return inventoryService;
+        }
+
+        private StoreService CreateStoreService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var storeService = new StoreService(userId);
+            return storeService;
+        }
+    }
+}
---
+++ FantasyStoreManager.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,74 @@
+﻿using FantasyStoreManager.Models;
+using FantasyStoreManager.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FantasyStoreManager.WebApi.Controllers
+{
+    [Authorize]
+    public class ProductController : ApiController
+    {
+        public IHttpActionResult GetAll()
+        {
+            var sortOrder = "";
+            ProductService productService = CreateProductService();
+            var products = productService.GetProducts(sortOrder);
+            return Ok(products);
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            ProductService productService = CreateProductService();
+            var product = productService.GetProductById(id);
+            return Ok(product);
+        }
+
+        public IHttpActionResult Post(ProductCreate product)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateProductService();
+
+            if (!service.CreateProduct(product))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Put(ProductEdit product)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateProductService();
+
+            if (!service.UpdateProduct(product))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateProductService();
+
+            if (!service.DeleteProduct(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private ProductService CreateProductService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var productService = new ProductService(userId);
+            return productService;
+        }
+    }
+}
